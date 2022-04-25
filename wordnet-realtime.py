c9b9d8ec76750f3ea3c76bdb4afa36b927463372
prxjju <pmaurya6jun@gmail.com>
2022-04-25 18:54:29 +0530

    realtime prediction

---
+++ wordnet-realtime.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import os
+# import pyttsx3
+#engine = pyttsx3.init()
+
+
+# load saved model from PC
+model = tf.keras.models.load_model('models/mobnet_model01.h5')
+model.summary()
+data_dir = 'Dataset/Static-Words'
+#getting the labels form data directory
+labels = sorted(os.listdir(data_dir))
+# labels[-1] = 'Nothing'
+# print(labels)
+
+#initiating the video source, 0 for internal camera
+cap = cv2.VideoCapture(0)
+while(True):
+    
+    _ , frame = cap.read()
+    cv2.rectangle(frame, (100, 100), (300, 300), (0, 0, 255), 5) 
+    #region of intrest
+    roi = frame[100:300, 100:300]
+    img = cv2.resize(roi, (96, 96))
+    cv2.imshow('roi', roi)
+    
+
+    img = preprocess_input(img)
+
+    #make predication about the current frame
+    prediction = model.predict(img.reshape(1,96,96,3))
+    char_index = np.argmax(prediction)
+    #print(char_index,prediction[0,char_index]*100)
+
+    confidence = round(prediction[0,char_index]*100, 1)
+    predicted_char = labels[char_index]
+
+    # Initialize the engine 
+    # engine = pyttsx3.init() 
+    # engine.say(predicted_char) 
+    # engine.runAndWait()
+
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    fontScale = 1
+    color = (0,255,255)
+    thickness = 2
+
+    #writing the predicted char and its confidence percentage to the frame
+    msg = predicted_char +', Conf: ' +str(confidence)+' %'
+    cv2.putText(frame, msg, (80, 80), font, fontScale, color, thickness)
+    
+    cv2.imshow('frame',frame)
+    
+    #close the camera when press 'q'
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+        
+#release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
